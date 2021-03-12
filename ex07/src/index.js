df7688ee94a2905eb7b7b37bf3c2afe8900b2cc9
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 23:52:24 +0100

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,11 @@
+function main() {
+    //Only change text below this line
+    var someAdjective = "the best!"
+    var myString = "Mistral is "
+    myString += someAdjective
+    //Only change text above this line
 
+    return myString;
+}
+console.log(main());
+module.exports = main;
