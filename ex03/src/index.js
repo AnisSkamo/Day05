cb009a58498e1cb28e6c2de9d9815408d91763e6
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 14:36:16 +0100

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,9 @@
+function main() {
+    //Only change text below this line
+    var myString = "FirstLine\n\tSecondLine\nThirdline"
+    //Only change text above this line
 
+    return myString;
+}
+console.log(main());
+module.exports = main;
