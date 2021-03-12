5dd0a1798cdf11e17c7b14ef5499a2e0e41580dc
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-13 00:18:21 +0100

    Add files via upload

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,10 @@
+function main() {
+    //Only change text below this line
+    var myString = "Jello World"
+    myString = "Hello World"
+    //Only change text above this line
 
+    return myString;
+}
+console.log(main());
+module.exports = main;
