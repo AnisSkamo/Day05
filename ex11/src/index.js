c6769e1cfcc67a8917f61617a0060f4cd4828704
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-13 00:25:08 +0100

    Add files via upload

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,1 +1,10 @@
+function main() {
+    //Only change text below this line
+    var lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2];
+    //Only change text above this line
 
+    return thirdLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
