31356291e15ffd62994693a770cec5cbe9eeb56f
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 14:26:08 +0100

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,9 @@
+function main() {
+    //Only change text below this line
+    var myLink = '<a href = "http://www.example.com" "target=blank>Link<a>"'
+    //Only change text above this line
 
+    return myLink;
+}
+console.log(main());
+module.exports = main;
