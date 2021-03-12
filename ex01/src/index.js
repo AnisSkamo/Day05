5ab9abafac52814df4fb5052a104400a80fa4fcc
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 14:10:53 +0100

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,13 @@
+function main() {
+    //Only change text below this line
+    var myFirstString = "I think therefore I\"am\" -- sentient.";
+    var mySecondString = "No, you only \"think\", so you aren\'t.";
+    //Only change text above this line
 
+    return{
+        myFirstString,
+        mySecondString,
+    };
+}
+console.log(main());
+module.exports = main;
