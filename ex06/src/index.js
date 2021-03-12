dbd17e7f15354f9be6103a19aea896d9605a96ee
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 14:55:20 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,10 @@
+function main() {
+    //Only change text below this line
+   var myName = "Anis Skamo";
+   var myString = "Hello, my name is " + myName + ", how are You?";
+    //Only change text above this line
 
+    return myString;
+}
+console.log(main());
+module.exports = main;
