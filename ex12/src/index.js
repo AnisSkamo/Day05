66086f66e34bfb758d47d3f1961140d4aa8fc3f3
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-13 00:30:05 +0100

    Add files via upload

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,1 +1,10 @@
+function main() {
+    //Only change text below this line
+    var lastName = "Tesla";
+    var lastLetterOfLastName = lastName[lastName.length - 1];
+    //Only change text above this line
 
+    return lastLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
