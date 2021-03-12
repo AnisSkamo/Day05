6e10a63644b2bc1b3622297841159283fcb8c232
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 14:50:24 +0100

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,11 @@
+function main() {
+    //Only change text below this line
+   var myString = "Concatentation is the process of appending"
+   myString += " "
+   myString += "one string to the end of another string."
+    //Only change text above this line
 
+    return myString;
+}
+console.log(main());
+module.exports = main;
