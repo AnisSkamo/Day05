0692951e0537e467c44e4bd709ee867b1fcf6799
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-13 00:00:22 +0100

    Add files via upload

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,10 @@
+function main() {
+    //Only change text below this line
+    var lastName = "Doe"
+    var lastNameLength = lastName.length
+    //Only change text above this line
 
+    return lastNameLength;
+}
+console.log(main());
+module.exports = main;
