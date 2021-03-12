eeee9de7a4846d5e2c2876e3bc77b8027011fd7e
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-13 00:09:13 +0100

    Add files via upload

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,11 @@
+function main() {
+    //Only change text below this line
+    var lastName = "Doe";
+    var firstLetterOfLastName = " ";
+    var firstLetterOfLastName = lastName [0];
+    //Only change text above this line
 
+    return firstLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
