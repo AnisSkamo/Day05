2ea3ae405d2bb7eacf2ed00434b7a0ba2d473573
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-13 00:34:38 +0100

    Add files via upload

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,1 +1,10 @@
+function main() {
+    //Only change text below this line
+    var lastName = "Bezos";
+    var thirdToLastLetterOfLastName = lastName[lastName.length - 3];
+    //Only change text above this line
 
+    return thirdToLastLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
