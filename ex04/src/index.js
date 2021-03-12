5d32fc07e73ecedcf9e9a149bb7895d7adb6474d
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 14:43:37 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,9 @@
+function main() {
+    //Only change text below this line
+    var myString = "This is the first string." + " " + "This is the second string.";
+    //Only change text above this line
 
+    return myString;
+}
+console.log(main());
+module.exports = main;
